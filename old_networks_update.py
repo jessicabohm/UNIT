f5cee6f33b4d9726a215c60a873e70b44062c1d9
jessica.bohm <j2bohm@uwaterloo.ca>
2025-09-22 20:13:11 -0600

    added prev best verison of VAE

---
+++ old_networks_update.py
@@ -0,0 +1,407 @@
+from torch import nn
+from torch.autograd import Variable
+import torch
+import torch.nn.functional as F
+try:
+    from itertools import izip as zip
+except ImportError: # will be 3.x series
+    pass
+
+# Adapted from UNIT paper - just updating to work on 3D rather than 2D
+
+
+##################################################################################
+# Encoder and Decoders
+##################################################################################
+
+# HARDCORE FOR NOW
+two_d = False # NOTE: 2D vs 3D
+n_downsample = 2
+dim=4
+img_x = 128
+img_y = 120
+img_z = 120
+latent_dim = 2000
+
+
+if two_d:
+    flattened_dim = dim*2**n_downsample*img_x/2**n_downsample*img_y/2**n_downsample
+else:
+    flattened_dim = dim*2**n_downsample*img_x/2**n_downsample*img_y/2**n_downsample*img_z/2**n_downsample
+
+flattened_dim = int(flattened_dim)
+
+
+
+# They had a Style and Content encoder - this content encoder just had resnet blocks instead
+# of global average pooling??
+class Encoder(nn.Module):
+    def __init__(self, n_downsample, n_res, input_dim, dim, norm, activ, pad_type):
+        super(Encoder, self).__init__()
+        self.model = []
+        if two_d:
+            self.model += [Conv2dBlock(input_dim, dim, 7, 1, 3, norm=norm, activation=activ, pad_type=pad_type)]
+        else:
+            self.model += [Conv3dBlock(input_dim, dim, 7, 1, 3, norm=norm, activation=activ, pad_type=pad_type)]
+
+        # downsampling blocks
+        for i in range(n_downsample):
+            if two_d:
+                self.model += [Conv2dBlock(dim, 2 * dim, 4, 2, 1, norm=norm, activation=activ, pad_type=pad_type)]
+            else:
+                self.model += [Conv3dBlock(dim, 2 * dim, 4, 2, 1, norm=norm, activation=activ, pad_type=pad_type)]
+            dim *= 2
+        # residual blocks
+        self.model += [ResBlocks(n_res, dim, norm=norm, activation=activ, pad_type=pad_type)]
+        self.model = nn.Sequential(*self.model)
+        self.output_dim = dim
+
+    def forward(self, x):
+        return self.model(x)
+
+class Decoder(nn.Module):
+    def __init__(self, n_upsample, n_res, dim, output_dim, res_norm='in', activ='relu', pad_type='zero'): # NOTE: updated normalization to in to not have to compute weight and bias externally
+        super(Decoder, self).__init__()
+
+        self.model = []
+        # AdaIN residual blocks # NOTE: changed!!
+        self.model += [ResBlocks(n_res, dim, res_norm, activ, pad_type=pad_type)]
+        # upsampling blocks
+        for i in range(n_upsample):
+            if two_d:
+                self.model += [nn.Upsample(scale_factor=2),
+                            Conv2dBlock(dim, dim // 2, 5, 1, 2, norm='in', activation=activ, pad_type=pad_type)] # NOTE: could update to instance norm since only a batch size of 2 -> don't want to normalize over full layer??
+            else:
+                self.model += [nn.Upsample(scale_factor=2),
+                    Conv3dBlock(dim, dim // 2, 5, 1, 2, norm='in', activation=activ, pad_type=pad_type)] # NOTE: could update to instance norm since only a batch size of 2 -> don't want to normalize over full layer??
+            
+            dim //= 2
+        # use reflection padding in the last conv layer
+        if two_d:
+            self.model += [Conv2dBlock(dim, output_dim, 7, 1, 3, norm='none', activation='none', pad_type=pad_type)] 
+        else:
+            self.model += [Conv3dBlock(dim, output_dim, 7, 1, 3, norm='none', activation='none', pad_type=pad_type)] 
+
+        self.model = nn.Sequential(*self.model)
+
+    def forward(self, x):
+        return self.model(x)
+    
+### Updated to be VAE style
+class Encoder_VAE(nn.Module):
+    def __init__(self, n_downsample, n_res, input_dim, dim, norm, activ, pad_type):
+        super(Encoder_VAE, self).__init__()
+        self.model = []
+        if two_d:
+            self.model += [Conv2dBlock(input_dim, dim, 7, 1, 3, norm=norm, activation=activ, pad_type=pad_type)]
+        else:
+            self.model += [Conv3dBlock(input_dim, dim, 7, 1, 3, norm=norm, activation=activ, pad_type=pad_type)]
+
+        # downsampling blocks
+        for i in range(n_downsample):
+            if two_d:
+                self.model += [Conv2dBlock(dim, 2 * dim, 4, 2, 1, norm=norm, activation=activ, pad_type=pad_type)]
+            else:
+                self.model += [Conv3dBlock(dim, 2 * dim, 4, 2, 1, norm=norm, activation=activ, pad_type=pad_type)]
+            dim *= 2
+
+        # residual blocks
+        self.model += [ResBlocks(n_res, dim, norm=norm, activation=activ, pad_type=pad_type)]
+
+        # NOTE: extra to map down to lower dim
+        self.model += [nn.Flatten(), nn.Linear(flattened_dim, latent_dim), nn.LayerNorm(latent_dim), nn.ReLU()]
+
+        self.output_dim = dim
+
+        # NOTE: dim might be incorrect here??
+        #self.inplace = nn.Linear(latent_dim, latent_dim)
+        #self.inplace = nn.Linear(flattened_dim, flattened_dim)
+        self.fc_mu = nn.Linear(latent_dim, latent_dim)       # outputs means
+        self.fc_logvar = nn.Linear(latent_dim, latent_dim)   # outputs log variances
+        self.model = nn.Sequential(*self.model)
+
+    def forward(self, x):
+        out = self.model(x)
+        means = self.fc_mu(out) # self.inplace(out)
+        log_vars = self.fc_logvar(out) #self.inplace(out) # why was it called log vars? Probs cuz of how it's used in KL divergence
+        return means, log_vars
+    
+class Reshape(nn.Module):
+    def __init__(self):
+        super().__init__()
+        #self.shape = (16, 16, 15, 15)  # e.g., (-1, 512) or (batch_size, channels, height, width)
+        self.shape = (dim*2**n_downsample, int(img_x/2**n_downsample), int(img_y/2**n_downsample), int(img_z/2**n_downsample))  # e.g., (-1, 512) or (batch_size, channels, height, width)
+
+    def forward(self, x):
+        return x.reshape(x.size(0), *self.shape)  # keeps batch dim intact
+
+class Decoder_VAE(nn.Module):
+    def __init__(self, n_upsample, n_res, dim, output_dim, res_norm='in', activ='relu', pad_type='zero'): # NOTE: updated normalization to in to not have to compute weight and bias externally
+        super(Decoder_VAE, self).__init__()
+
+        self.model = []
+
+        self.model += [nn.Linear(latent_dim, flattened_dim), nn.LayerNorm(flattened_dim), nn.ReLU(), Reshape()]
+        #self.model += [Reshape()]
+        
+        # AdaIN residual blocks # NOTE: changed!!
+        self.model += [ResBlocks(n_res, dim, res_norm, activ, pad_type=pad_type)]
+
+        # upsampling blocks
+        for i in range(n_upsample):
+            if two_d:
+                self.model += [nn.Upsample(scale_factor=2),
+                            Conv2dBlock(dim, dim // 2, 5, 1, 2, norm='in', activation=activ, pad_type=pad_type)] # NOTE: could update to instance norm since only a batch size of 2 -> don't want to normalize over full layer??
+            else:
+                self.model += [nn.Upsample(scale_factor=2),
+                    Conv3dBlock(dim, dim // 2, 5, 1, 2, norm='in', activation=activ, pad_type=pad_type)] # NOTE: could update to instance norm since only a batch size of 2 -> don't want to normalize over full layer??
+            
+            dim //= 2
+        # use reflection padding in the last conv layer
+        if two_d:
+            self.model += [Conv2dBlock(dim, output_dim, 7, 1, 3, norm='none', activation='none', pad_type=pad_type)] 
+        else:
+            self.model += [Conv3dBlock(dim, output_dim, 7, 1, 3, norm='none', activation='none', pad_type=pad_type)] 
+
+        # use reflection padding in the last conv layer
+        self.model = nn.Sequential(*self.model)
+
+    def forward(self, x):
+        return self.model(x)
+    
+
+
+
+##################################################################################
+# Sequential Models
+##################################################################################
+class ResBlocks(nn.Module):
+    def __init__(self, num_blocks, dim, norm='in', activation='relu', pad_type='zero'):
+        super(ResBlocks, self).__init__()
+        self.model = []
+        for i in range(num_blocks):
+            self.model += [ResBlock(dim, norm=norm, activation=activation, pad_type=pad_type)]
+        self.model = nn.Sequential(*self.model) # just unpacks the layers and makes them torch nn layers
+
+    def forward(self, x):
+        return self.model(x)
+    
+
+##################################################################################
+# Basic Blocks
+##################################################################################
+class ResBlock(nn.Module):
+    def __init__(self, dim, norm='in', activation='relu', pad_type='zero'):
+        super(ResBlock, self).__init__()
+
+        model = []
+
+        if two_d:
+            model += [Conv2dBlock(dim ,dim, 3, 1, 1, norm=norm, activation=activation, pad_type=pad_type)]
+            model += [Conv2dBlock(dim ,dim, 3, 1, 1, norm=norm, activation='none', pad_type=pad_type)]
+        else:
+            model += [Conv3dBlock(dim ,dim, 3, 1, 1, norm=norm, activation=activation, pad_type=pad_type)]
+            model += [Conv3dBlock(dim ,dim, 3, 1, 1, norm=norm, activation='none', pad_type=pad_type)]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        residual = x
+        out = self.model(x)
+        out += residual
+        return out
+
+class Conv3dBlock(nn.Module):
+    def __init__(self, input_dim ,output_dim, kernel_size, stride,
+                 padding=0, norm='none', activation='relu', pad_type='zero'):
+        super(Conv3dBlock, self).__init__()
+        self.use_bias = True
+        # initialize padding
+        if pad_type == 'reflect':
+            self.pad = nn.ReflectionPad3d(padding)
+        elif pad_type == 'replicate':
+            self.pad = nn.ReplicationPad3d(padding)
+        elif pad_type == 'zero':
+            self.pad = nn.ConstantPad3d(padding, value=0)
+        else:
+            assert 0, "Unsupported padding type: {}".format(pad_type)
+
+        # initialize normalization
+        norm_dim = output_dim
+        if norm == 'bn':
+            self.norm = nn.BatchNorm3d(norm_dim)
+        elif norm == 'in':
+            #self.norm = nn.InstanceNorm3d(norm_dim, track_running_stats=True)
+            self.norm = nn.InstanceNorm3d(norm_dim)
+        elif norm == 'ln':
+            self.norm = LayerNorm(norm_dim)
+        elif norm == 'adain':
+            self.norm = AdaptiveInstanceNorm3d(norm_dim)
+        elif norm == 'none':
+            self.norm = None
+        else:
+            assert 0, "Unsupported normalization: {}".format(norm)
+
+        # initialize activation
+        if activation == 'relu':
+            self.activation = nn.ReLU(inplace=True)
+        elif activation == 'lrelu':
+            self.activation = nn.LeakyReLU(0.2, inplace=True)
+        elif activation == 'prelu':
+            self.activation = nn.PReLU()
+        elif activation == 'selu':
+            self.activation = nn.SELU(inplace=True)
+        elif activation == 'tanh':
+            self.activation = nn.Tanh()
+        elif activation == 'none':
+            self.activation = None
+        else:
+            assert 0, "Unsupported activation: {}".format(activation)
+
+        # initialize convolution
+        self.conv = nn.Conv3d(input_dim, output_dim, kernel_size, stride, bias=self.use_bias) # NOTE: just updated this to be 3d?? is that ok??
+
+    def forward(self, x):
+        x = self.conv(self.pad(x))
+        if self.norm:
+            x = self.norm(x)
+        if self.activation:
+            x = self.activation(x)
+        return x
+    
+class Conv2dBlock(nn.Module):
+    def __init__(self, input_dim ,output_dim, kernel_size, stride,
+                 padding=0, norm='none', activation='relu', pad_type='zero'):
+        super(Conv2dBlock, self).__init__()
+        self.use_bias = True
+        # initialize padding
+        if pad_type == 'reflect':
+            self.pad = nn.ReflectionPad2d(padding)
+        elif pad_type == 'replicate':
+            self.pad = nn.ReplicationPad2d(padding)
+        elif pad_type == 'zero':
+            self.pad = nn.ZeroPad2d(padding)
+        else:
+            assert 0, "Unsupported padding type: {}".format(pad_type)
+
+        # initialize normalization
+        norm_dim = output_dim
+        if norm == 'bn':
+            self.norm = nn.BatchNorm2d(norm_dim)
+        elif norm == 'in':
+            #self.norm = nn.InstanceNorm2d(norm_dim, track_running_stats=True)
+            self.norm = nn.InstanceNorm2d(norm_dim)
+        elif norm == 'ln':
+            self.norm = LayerNorm(norm_dim)
+        elif norm == 'adain':
+            self.norm = AdaptiveInstanceNorm2d(norm_dim)
+        elif norm == 'none':
+            self.norm = None
+        else:
+            assert 0, "Unsupported normalization: {}".format(norm)
+
+        # initialize activation
+        if activation == 'relu':
+            self.activation = nn.ReLU(inplace=True)
+        elif activation == 'lrelu':
+            self.activation = nn.LeakyReLU(0.2, inplace=True)
+        elif activation == 'prelu':
+            self.activation = nn.PReLU()
+        elif activation == 'selu':
+            self.activation = nn.SELU(inplace=True)
+        elif activation == 'tanh':
+            self.activation = nn.Tanh()
+        elif activation == 'none':
+            self.activation = None
+        else:
+            assert 0, "Unsupported activation: {}".format(activation)
+
+        # initialize convolution
+        self.conv = nn.Conv2d(input_dim, output_dim, kernel_size, stride, bias=self.use_bias)
+
+    def forward(self, x):
+        x = self.conv(self.pad(x))
+        if self.norm:
+            x = self.norm(x)
+        if self.activation:
+            x = self.activation(x)
+        return x
+    
+
+##################################################################################
+# Normalization layers
+##################################################################################
+class AdaptiveInstanceNorm2d(nn.Module):
+    def __init__(self, num_features, eps=1e-5, momentum=0.1):
+        super(AdaptiveInstanceNorm2d, self).__init__()
+        self.num_features = num_features
+        self.eps = eps
+        self.momentum = momentum
+        # weight and bias are dynamically assigned
+        self.weight = None
+        self.bias = None
+        # just dummy buffers, not used
+        self.register_buffer('running_mean', torch.zeros(num_features))
+        self.register_buffer('running_var', torch.ones(num_features))
+
+# NOTE: why was it 2d???
+class AdaptiveInstanceNorm3d(nn.Module):
+    def __init__(self, num_features, eps=1e-5, momentum=0.1):
+        super(AdaptiveInstanceNorm3d, self).__init__()
+        self.num_features = num_features
+        self.eps = eps
+        self.momentum = momentum
+        # weight and bias are dynamically assigned
+        self.weight = None
+        self.bias = None
+        # just dummy buffers, not used
+        self.register_buffer('running_mean', torch.zeros(num_features))
+        self.register_buffer('running_var', torch.ones(num_features))
+
+    def forward(self, x):
+        assert self.weight is not None and self.bias is not None, "Please assign weight and bias before calling AdaIN!"
+        b, c = x.size(0), x.size(1)
+        running_mean = self.running_mean.repeat(b)
+        running_var = self.running_var.repeat(b)
+
+        # Apply instance norm
+        x_reshaped = x.contiguous().view(1, b * c, *x.size()[2:])
+
+        out = F.batch_norm(
+            x_reshaped, running_mean, running_var, self.weight, self.bias,
+            True, self.momentum, self.eps)
+
+        return out.view(b, c, *x.size()[2:])
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' + str(self.num_features) + ')'
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, num_features, eps=1e-5, affine=True):
+        super(LayerNorm, self).__init__()
+        self.num_features = num_features
+        self.affine = affine
+        self.eps = eps
+
+        if self.affine:
+            self.gamma = nn.Parameter(torch.Tensor(num_features).uniform_())
+            self.beta = nn.Parameter(torch.zeros(num_features))
+
+    def forward(self, x):
+        shape = [-1] + [1] * (x.dim() - 1)
+        # print(x.size())
+        if x.size(0) == 1:
+            # These two lines run much faster in pytorch 0.4 than the two lines listed below.
+            mean = x.view(-1).mean().view(*shape)
+            std = x.view(-1).std().view(*shape)
+        else:
+            mean = x.view(x.size(0), -1).mean(1).view(*shape)
+            std = x.view(x.size(0), -1).std(1).view(*shape)
+
+        x = (x - mean) / (std + self.eps)
+
+        if self.affine:
+            shape = [1, -1] + [1] * (x.dim() - 2)
+            x = x * self.gamma.view(*shape) + self.beta.view(*shape)
+        return x
---
+++ old_train_VAE.py
@@ -0,0 +1,267 @@
+from torch.utils.data import Dataset
+import torch
+import SimpleITK as sitk
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from old_networks_update import *
+import csv
+import time
+import torchvision.utils as vutils
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap, BoundaryNorm
+
+
+# Dataset path
+train_folder = "./datasets/anat/human_train/"
+train_paths = [train_folder + file_name for file_name in os.listdir(train_folder)]
+
+train_paths = train_paths
+
+val_folder = "./datasets/anat/human_test/"
+val_paths = [val_folder + file_name for file_name in os.listdir(val_folder)]
+val_paths.sort()
+
+# folder to save model checkpoints
+train_save_folder = "./VAE_train/3d_anat/testing_human_best/"
+
+os.makedirs(train_save_folder + "/test_images", exist_ok=True)
+
+# file to save losses
+csv_file = 'loss_log.csv'
+
+# epoch to load from
+start_epoch = 0
+
+# num epoch to train for
+num_epochs = 1000
+
+# how often to save model checkpoints and images
+save_imgs = True
+save_imgs_freq = 5
+save_model_freq = 10
+
+lr=1e-4
+batch_size= 4
+
+###################################################################################################################
+################################################################################################################### finish setting some params
+
+# Loss function for VAE
+def loss_func(imgs, recons, means, log_vars):
+    criterion = nn.CrossEntropyLoss()
+    recon = criterion(recons, imgs) # computes average per voxel (in CVAE they use this instead to sum over all voxels)
+
+    BS = batch_size
+    num_voxels = 120*120*128 # NOTE: update for 3D
+    #num_voxels = 120*120 # NOTE: update for 2D
+    beta = 10
+    KLD = (-0.5 * torch.sum(1 + log_vars - means.pow(2) - log_vars.exp())) / (num_voxels * BS)
+
+    return recon + beta*KLD
+
+def reparameterization(means, log_vars):
+    # move random vars sampled from a normal dist to size log_vars to device
+    epsilon = torch.randn_like(log_vars).to(device) 
+    std = torch.exp(0.5 * log_vars)
+    z = means + std * epsilon
+    return z
+
+class Segmentation3DDataset(Dataset):
+    def __init__(self, image_paths, transform=None):
+        self.image_paths = image_paths
+        self.transform = transform  # Optional (e.g., normalization, crop)
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image = sitk.ReadImage(self.image_paths[idx])
+        image = sitk.GetArrayFromImage(image)
+
+        image = np.expand_dims(image, axis=0)
+        if self.transform:
+            image = self.transform(image)
+
+        return torch.from_numpy(image).to(torch.float)
+
+two_d = False
+
+# ===== Your consistent color setup =====
+base_colors = plt.cm.get_cmap('tab20').colors  # 20 RGBA colors
+n_labels = 300
+repeated_colors = np.tile(base_colors, (n_labels // 20 + 1, 1))[:n_labels]
+cmap = ListedColormap(repeated_colors)
+n_labels = 270
+norm = BoundaryNorm(np.arange(n_labels + 1), cmap.N)
+
+# Manually set label 0 to white
+colors_with_white_bg = cmap.colors
+colors_with_white_bg[0] = (1.0, 1.0, 1.0)  # RGB white
+cmap = ListedColormap(colors_with_white_bg)
+
+def __write_images(image_outputs, display_image_num, file_name):
+    imgs, recons = image_outputs
+
+    # If 3D volumes: [B, D, H, W] → take middle slice
+    if imgs.ndim == 4:
+        slice_idx = imgs.shape[1] // 2
+        imgs = torch.stack([img[slice_idx] for img in imgs[:display_image_num]])
+        recons = torch.stack([img[slice_idx] for img in recons[:display_image_num]])
+    else:
+        imgs = imgs[:display_image_num]
+        recons = recons[:display_image_num]
+
+    # Apply discrete colormap and return RGB tensors
+    def apply_cmap_rgb(tensor):
+        arr = tensor.cpu().numpy().astype(np.int32)
+        rgb_list = []
+        for img in arr:
+            rgb_img = cmap(norm(img))[..., :3]  # drop alpha
+            rgb_tensor = torch.from_numpy(rgb_img).permute(2, 0, 1)  # [3, H, W]
+            rgb_list.append(rgb_tensor)
+        return torch.stack(rgb_list)
+
+    imgs_rgb = apply_cmap_rgb(imgs)
+    recons_rgb = apply_cmap_rgb(recons)
+
+    # Create grids
+    grid_in = vutils.make_grid(imgs_rgb, nrow=display_image_num, padding=2)
+    grid_rec = vutils.make_grid(recons_rgb, nrow=display_image_num, padding=2)
+
+    # Stack vertically
+    full_grid = torch.cat([grid_in, grid_rec], dim=1)
+
+    vutils.save_image(full_grid, file_name)
+    
+# Create dataset and dataloader
+train_dataset = Segmentation3DDataset(image_paths=train_paths)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+val_dataset = Segmentation3DDataset(image_paths=val_paths)
+val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
+
+# Initialize model
+encoder = Encoder_VAE(n_downsample=2, n_res=1, input_dim=1, dim=4, norm='in', activ='relu', pad_type='zero') # encodes to 32 dim??
+decoder = Decoder_VAE(n_upsample=2, n_res=1, dim=encoder.output_dim, output_dim=271)
+
+# Move to GPU if available
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+encoder.to(device)
+decoder.to(device)
+
+# if not training from 0 load a pretrained model from saved checkpoint
+if start_epoch != 0:
+    encoder.load_state_dict(torch.load(train_save_folder + "checkpoint_epoch_" + str(start_epoch))['encoder_state_dict'])
+    decoder.load_state_dict(torch.load(train_save_folder + "checkpoint_epoch_" + str(start_epoch))['decoder_state_dict'])
+
+# Loss function
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr=lr)
+
+save_loss = []
+
+# for saving images during training
+display_size = 16 # num images to display
+
+print("Start training!!")
+for epoch in range(start_epoch, start_epoch + num_epochs + 1):
+    epoch_start_time = time.time()
+    encoder.train()
+    decoder.train()
+    train_loss = 0
+
+    for batch in train_loader:
+        batch = batch.to(device)  # (B, C, D, H, W)
+
+        # Forward pass
+        means, log_vars = encoder(batch)
+
+        # get latent vectors - sampled from learned dists
+        z = reparameterization(means, log_vars)
+
+        recon = decoder(z)
+
+        # Compute loss
+        loss = loss_func(batch.squeeze(1).long(), recon, means, log_vars)
+
+
+        # Backprop
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+
+    encoder.eval()
+    decoder.eval()
+    val_loss = 0
+
+    img_to_save = []
+    recon_to_save = []
+
+    with torch.no_grad():
+        for i, val_batch in enumerate(val_loader):
+            val_batch = val_batch.to(device)
+
+            means, log_vars = encoder(val_batch)
+            # get latent vectors - sampled from learned dists
+            z = reparameterization(means, log_vars)
+            recon = decoder(z)
+            # Compute loss
+            loss = loss_func(val_batch.squeeze(1).long(), recon, means, log_vars)
+
+            val_loss += loss.item()
+
+            # save a few test images
+            if save_imgs and (epoch % save_imgs_freq == 0) and i < 3:
+                recon = torch.argmax(recon, dim=1).squeeze().detach().cpu()
+                recon_to_save.append(recon) # might just be a shallow copy
+
+                #recon_img = sitk.GetImageFromArray(np.array(recon))
+                # save a few nifty image reconstructions - use for analysis
+                #sitk.WriteImage(recon_img, train_save_folder + "test_images/recon_" + val_paths[i].split("/")[-1].split(".")[0] + "_epoch_" + str(epoch) + ".nii")
+
+                # to display
+                img_to_save.append(val_batch)
+
+            if save_imgs and i >= 3 and i < display_size and (epoch % save_imgs_freq == 0):
+                recon = torch.argmax(recon, dim=1).squeeze().detach().cpu()
+                recon_to_save.append(recon)
+                img_to_save.append(val_batch)
+
+        # save a png of some reconstructions - to observe during training
+        if save_imgs and epoch % save_imgs_freq == 0:
+            img_to_save = torch.stack(img_to_save).squeeze()
+            recon_to_save = torch.stack(recon_to_save)
+
+            print("save images...")
+            __write_images([img_to_save, recon_to_save], display_size, train_save_folder + "test_images/recons_epoch_" + str(epoch) + ".png")
+
+
+    train_loss = train_loss / len(train_loader)
+    val_loss = val_loss / len(val_loader)
+    elapsed_time = time.time() - epoch_start_time
+    print(f"Epoch [{epoch+1}/{num_epochs}], Train loss: {train_loss:.4f}, Val loss: {val_loss:.4f} (time: {elapsed_time:.4f})")
+    
+    # write loss to a csv every epoch
+    with open(train_save_folder + 'loss_log.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([epoch, train_loss, val_loss])  # Writes a single row with two values
+
+    # save model checkpoint
+    if epoch % save_model_freq == 0:
+        checkpoint = {
+            'epoch': epoch,
+            'encoder_state_dict': encoder.state_dict(),
+            'decoder_state_dict': decoder.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'train_loss': train_loss,
+            'test_loss': val_loss,
+        }
+
+        torch.save(checkpoint, train_save_folder + "checkpoint_epoch_" + str(epoch))
